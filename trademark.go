50af3440cc9fd5ebca1ed889a34b99c611c0f5f5
Kevin Ip <28875408+kevin-ip@users.noreply.github.com>
2025-02-06 22:37:40 -0600

    introduce NewTrademarkService()

--- trademark.go
+++ trademark.go
@@ -1,9 +1,17 @@
 package go_markerapi_client
 
-import "context"
+import (
+	"context"
+
+	"github.com/kevin-ip/go-trademark-api-client/uspto_trademark"
+)
 
 type TrademarkService interface {
-	// IsAvailable Check whether the search term is available
-	// True if available, false otherwise
+	// IsAvailable checks whether the search term is available and free to use
+	// True if available, false if trademarked
 	IsAvailable(ctx context.Context, searchTerm string) (bool, error)
+}
+
+func NewTrademarkService(vendor string, apiKey string) TrademarkService {
+	return uspto_trademark.NewUSPTOTradeMarkService(apiKey)
 }
